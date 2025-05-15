6369192e1997829a9d8682765aca93bedcbb7e03
Asisah134 <asisahnurul234@gmail.com>
2025-05-15 19:58:21 +0700

    Add files via upload

---
+++ soal no1.js
@@ -0,0 +1,8 @@
+// Problem 1 - Penggabungan String
+let firstName = 'Asisah';
+let midleName = 'Nurul'
+let lastName = 'Ilmi';
+let bornYear = 2006;
+const currentYear = 2025;
+const age = currentYear - bornYear;
+console.log(`Halo, ${firstName} ${lastName}! Anda berusia ${age} tahun.`);
