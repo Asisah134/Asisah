aeaa3484cbc11ad68d4d36e14d30877739abe0f0
Asisah134 <asisahnurul234@gmail.com>
2025-05-15 21:17:23 +0700

    Add files via upload

---
+++ soalno3.js
@@ -0,0 +1,10 @@
+let panjang = 20;
+let lebar = 5;
+
+// Menghitung luas persegi panjang (panjang x lebar)
+let luas = panjang * lebar;
+console.log('Luas Persegi Panjang = ' + luas);
+
+// Menghitung keliling persegi panjang (2 x (panjang + lebar))
+let keliling = 2 * (panjang + lebar);
+console.log('Keliling Persegi Panjang = ' + keliling);
